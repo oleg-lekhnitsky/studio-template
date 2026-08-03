4b8c86e1f49b31812d633a3f345704fcd4b8ae47
Oleg Lekhnitsky <oleg-lekhnitsky@users.noreply.github.com>
2026-08-03 13:51:44 +0300

    sanity logs

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -4,5 +4,8 @@ export default defineCliConfig({
   api: {
     projectId: process.env.SANITY_STUDIO_PROJECT_ID || 'v8alcn6p',
     dataset: process.env.SANITY_STUDIO_DATASET || 'production'
+  },
+  deployment: {
+    appId: 'wki8yzuho8mnjmvuvmiwmr0g'
   }
 })
