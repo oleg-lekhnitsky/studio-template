f1363566a184fe3966b48d8108c01d10bd29c927
Oleg Lekhnitsky <oleg-lekhnitsky@users.noreply.github.com>
2026-08-04 15:55:20 +0300

    config

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -6,9 +6,12 @@ import { schemaTypes } from './schemaTypes'
 
 const siteSettingsId = '4e409066-8870-4256-9bd0-a6b3b930ec39'
 const projectId = process.env.SANITY_STUDIO_PROJECT_ID || 'v8alcn6p'
-const defaultPreviewUrl = process.env.SANITY_STUDIO_PREVIEW_URL || 'http://localhost:3000'
-const productionPreviewUrl = process.env.SANITY_STUDIO_PREVIEW_URL_PRODUCTION || defaultPreviewUrl
-const versionTwoPreviewUrl = process.env.SANITY_STUDIO_PREVIEW_URL_VERSION_TWO || defaultPreviewUrl
+const isLocalStudio = process.env.NODE_ENV === 'development'
+const localPreviewUrl = process.env.SANITY_STUDIO_PREVIEW_URL || 'http://localhost:3000'
+const productionPreviewUrl = process.env.SANITY_STUDIO_PREVIEW_URL_PRODUCTION
+  || (isLocalStudio ? localPreviewUrl : 'https://studio-template-studio-ly5d.vercel.app')
+const versionTwoPreviewUrl = process.env.SANITY_STUDIO_PREVIEW_URL_VERSION_TWO
+  || (isLocalStudio ? localPreviewUrl : 'https://yuliana-two.vercel.app')
 
 const mainDocuments = defineDocuments([
   {
